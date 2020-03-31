df27f245639f9ec9ebcb33c06dd394e9a4913d0b
Viktor <Vituna9@bk.ru>
2020-04-01 00:00:38 +0300

    Добавляет разметку_2

--- src/main.js
+++ src/main.js
@@ -317,8 +317,8 @@ const createRoutePointTemplate = () => {
   );
 };
 
-const render = (container, template, place) => {
-  container.insertAdjacentHTML(place, template, place);
+const render = (container, template, place = `beforeEnd`) => {
+  container.insertAdjacentHTML(place, template);
 };
 
 const siteHeaderElement = document.querySelector(`.page-header`);
@@ -327,21 +327,21 @@ const siteRouteElement = siteHeaderElement.querySelector(`.trip-main`);
 render(siteRouteElement, createSiteRouteInformation(), `afterBegin`);
 
 const siteCostElement = siteHeaderElement.querySelector(`.trip-main__trip-info`);
-render(siteCostElement, createSiteCostTravel(), `beforeEnd`);
+render(siteCostElement, createSiteCostTravel());
 
 const siteMenuElement = siteHeaderElement.querySelector(`.visually-hidden`);
 render(siteMenuElement, createSiteMenuTemplate(), `afterEnd`);
 
 const siteFilterElement = siteHeaderElement.querySelector(`.trip-main__trip-controls`);
-render(siteFilterElement, createFilterTemplate(), `beforeEnd`);
+render(siteFilterElement, createFilterTemplate());
 
 const siteMainElement = document.querySelector(`.page-body__page-main`);
 
 const siteSortingElement = siteMainElement.querySelector(`.trip-events`);
-render(siteSortingElement, createSortingTemplate(), `beforeEnd`);
-render(siteSortingElement, createCreateFormTemplate(), `beforeEnd`);
+render(siteSortingElement, createSortingTemplate());
+render(siteSortingElement, createCreateFormTemplate());
 
 for (let i = 0; i < TASK_COUNT; i++) {
-  render(siteSortingElement, createRoutePointTemplate(), `beforeEnd`);
+  render(siteSortingElement, createRoutePointTemplate());
 }
 
